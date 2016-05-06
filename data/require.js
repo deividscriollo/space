ae927982e374a176982f9854779bdd6cb6a5309b
Deivid <deividscriollo@gmail.com>
2016-05-06 12:43:28 -0500

    new add

---
+++ data/require.js
@@ -0,0 +1,15 @@
+$(function(){
+	// =========================================== Inlude controller acction ===================================================//
+	var path = 'data/';
+	$.getScript(path + "home/controller/app.js");
+	$.getScript(path + "inicio/controller/app.js");
+	$.getScript(path + "registro/controller/app.js");
+	$.getScript(path + "session/controller/app.js");
+	// $.getScript(path + "home/controller/.js");
+	// $.getScript(path + "home/controller/.js");
+	// $.getScript(path + "home/controller/.js");
+	// $.getScript(path + "home/controller/.js");
+
+	// var path = 'data/space';
+	// $.getScript(path + "home/controller/.js");
+});
